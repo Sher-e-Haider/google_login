21fd674d7d627e7eab78a636dc65045df2a43c7f
md sahil raza <razasahil263@gmail.com>
2022-02-17 15:04:40 +0530

    deploy to heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import { GoogleLogin } from 'react-google-login'
+
+
 
 function App() {
+  const [loginData,setLoginData] = useState(
+    localStorage.getItem('loginData')
+      ? JSON.parse(localStorage.getItem('loginData'))
+      :null
+  );
+  
+  const handleFailure=(res)=>{
+      alert(res)
+  }
+  const handleLogin=async(googleData)=>{
+       const res = await fetch('/api/google-login',{
+         method:'POST',
+         body: JSON.stringify({
+           token:googleData.tokenId,
+         }),
+         headers:{
+           'Content-Type':'application/json'
+         }
+       })
+       const data = await res.json()
+       setLoginData(data)
+       localStorage.setItem('loginData',JSON.stringify(data))
+  }
+  const handleLogout=()=>{
+    localStorage.removeItem('loginData')
+    setLoginData(null)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>React Google Login App</h1>
+       <div>
+         {
+           loginData?(
+             <div>
+               <h3>You logged in as {loginData.email}</h3>
+               <button onClick={handleLogout} >Logout</button>
+             </div>
+              
+           ):(
+              <GoogleLogin 
+                      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                      buttonText="Log in with google"
+                      onSuccess={handleLogin}
+                      onFailure={handleFailure}
+                     
+                    >
+              </GoogleLogin> 
+           )
+         }
+        
+       </div>
+       
+       </header>
+    
     </div>
   );
 }
